92a5323a89336864f03323a5b13e7b082059d43d
mmcdonald3009 <102558194+mmcdonald3009@users.noreply.github.com>
2022-03-29 09:08:04 +1100

    Create gtkthemesync.sh

---
+++ gtkthemesync.sh
@@ -0,0 +1,27 @@
+if ! pidof inotifywait;then
+inotifywait --quiet --monitor --event close_write ~/.config/dconf/ | while read; do
+
+dconf write /org/gtk/settings/file-chooser/show-hidden false
+
+colorscheme=$(gsettings get org.gnome.desktop.interface color-scheme);
+gtktheme=$(gsettings get org.gnome.desktop.interface gtk-theme);
+
+
+
+if [ $colorscheme = "'prefer-dark'" ]; then
+if [ ! $gtktheme = "'Adwaita-dark'" ]; then
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings get org.gnome.shell.extensions.user-theme name 'CustomDark'
+fi
+fi
+
+
+if [ $colorscheme = "'default'" ]; then
+if [ ! $gtktheme = "'Adwaita'" ]; then
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
+gsettings get org.gnome.shell.extensions.user-theme name 'CustomLight'
+fi
+fi
+
+done
+fi
