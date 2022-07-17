c2081c4287a9733c936f85ca44c2f25624dab2e5
mmcdonald3009 <102558194+mmcdonald3009@users.noreply.github.com>
2022-07-18 08:47:42 +1000

    Create gtkthemesync.sh
    The original solution was tested on openSUSE Tumbelweed only, There is now a solution for Ubuntu 22.04 in the code also ...use one or the other

--- gtkthemesync.sh
+++ gtkthemesync.sh
@@ -1,3 +1,8 @@
+#### THIS TESTED ON openSUSE TUMBLEWEED, SEE NEXT BELOW FOR AN UBUNTU SOLUTION - USE one or the other...
+
+
+### USE THIS ONE FOR openSUSE ###
+
 if ! pidof inotifywait;then
 inotifywait --quiet --monitor --event close_write ~/.config/dconf/ | while read; do
 
@@ -25,3 +30,19 @@ fi
 
 done
 fi
+
+### END openSUSE ###
+
+
+### USE THIS FOR UBUNTU 22.04 ###
+
+if ! pidof inotifywait; then
+while inotifywait -e modify ~/.config/dconf; do
+
+dconf write /org/gtk/settings/file-chooser/show-hidden false
+
+done
+fi
+
+### END UBUNTU 22.04 ###
+
